c7ae90383c182cf45bac4842787c8e646ceb9743
Ze1ex <moses49618@gmail.com>
2024-05-27 14:39:24 +0700

    first commit

---
+++ app1.py
@@ -0,0 +1,4 @@
+sayHi = 'welcome to python'
+name = 'Mr.Mateo'
+age = 15
+print(f'{sayHi} Name:{name} Age:{age}')
---
+++ app6.py
@@ -0,0 +1,8 @@
+#Useing input() to take user input
+#num = input("Enter a number:")
+banana = int(input("Enter a banana:"))
+lemon = int(input("Enter a lemon:"))
+
+fruit = banana + lemon
+#print(f'You Entered: {num}',type(num))
+print(f'You Entered: {banana + lemon}',type(fruit))
